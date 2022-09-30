217a6621e4235ac3d02f328e8e70f50c9c1d82bb
Bode <jry.bode@gmail.com>
2022-09-30 09:36:30 +0700

    Add files via upload

---
+++ Lec8/php_workshop2/operation.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<?php
+$a= $_POST["a"];
+$b = $_POST["b"];
+echo $a+$b . "<br>";
+echo $a-$b . "<br>";
+echo $a*$b . "<br>";
+echo $a/$b . "<br>";
+?>
+</body>
+</html>
---
+++ Lec8/test/test3.php
@@ -0,0 +1,4 @@
+<?php
+$person = array("fullname"=>"John", "height"=>"173", "age"=>"43");
+print_r($person);
+?>
